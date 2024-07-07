21381a27b3a46a8ee39963b2a810bbbf7f402995
Nexs12 <152890031+Nexs12@users.noreply.github.com>
2024-07-07 17:48:51 +0300

    box-2

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -2,13 +2,16 @@ import css from "./Feedback.module.css";
 
 const Feedback = () => {
     return (
-        <ul className={css.feedbackList}>
-            <li>Good: </li>
-            <li>Neutral:</li>
-            <li>Bad:</li>
-            <li>Total:</li>
-            <li>positive:</li>
-        </ul>
+       <div>
+            <p>No feedback yet</p>
+            <ul className={css.feedbackList}>
+                <li>Good: </li>
+                <li>Neutral:</li>
+                <li>Bad:</li>
+                <li>Total:</li>
+                <li>positive:</li>
+            </ul>
+       </div>
     )
 
 }
